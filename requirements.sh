abc796bf3c1714eeb4c44b7628adb74f610465e4
samrat-halder <sh3970@columbia.edu>
2020-04-28 03:57:18 +0000

    minor fix

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 mkdir model_folder
-#mkdir data
+mkdir data
 cd model_folder
 wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
 wget https://raw.githubusercontent.com/google-research/bert/master/modeling.py
