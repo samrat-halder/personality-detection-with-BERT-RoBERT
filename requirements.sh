6c9bdca0c5ea1080da7206ef84976461d8e60e30
samrat-halder <sh3970@columbia.edu>
2020-05-03 04:24:34 +0000

    add setup files for server

--- requirements.sh
+++ requirements.sh
@@ -1,14 +1,15 @@
-mkdir model_folder
-mkdir data
+#mkdir model_folder
+mkdir -p data
 cd model_folder
 wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
-wget https://raw.githubusercontent.com/google-research/bert/master/modeling.py
-wget https://raw.githubusercontent.com/google-research/bert/master/optimization.py
-wget https://raw.githubusercontent.com/google-research/bert/master/run_classifier.py
-wget https://raw.githubusercontent.com/google-research/bert/master/tokenization.py
+#wget https://raw.githubusercontent.com/google-research/bert/master/modeling.py
+#wget https://raw.githubusercontent.com/google-research/bert/master/optimization.py
+#wget https://raw.githubusercontent.com/google-research/bert/master/run_classifier.py
+#wget https://raw.githubusercontent.com/google-research/bert/master/tokenization.py
 sudo apt-get intall python3-dev
 sudo apt-get install build-essential
 sudo apt-get install ipython3
 sudo apt-get install python3-pip
 python3 -m pip install pandas
 python3 -m pip install nltk
+python3 -m pip install tensorflow-gpu==1.11
