d66b51830d8cdba82b23733d23d64a2ffaaf0b99
samrat-halder <sh3970@columbia.edu>
2020-05-07 12:13:41 +0000

    added functionality in data prep

--- src/data_prep.py
+++ src/data_prep.py
@@ -9,32 +9,35 @@ import copy
 
 seq_length = 150
 overlap_length = 25
-n_sample = 2#1000
+n_sample = 999999
+all_class = True
 
 data_path = '/home/oblivion/mbti-personality-detection/data'
 filename = 'mbti9k_comments.csv'
-split_sent_filename = 'training_data_sample_' + str(seq_length) + '_' + str(n_sample) + '.pkl'
-split_sent_filename_2 = 'training_data_sample_h_' + str(seq_length) + '_' + str(n_sample) + '.pkl'
+split_sent_filename = 'training_data_sample_' + str(seq_length) + '_' + str(n_sample) + '_' + str(all_class) + '.pkl'
+split_sent_filename_h = 'training_data_sample_h_' + str(seq_length) + '_' + str(n_sample) + '_'+ str(all_class)+ '.pkl'
 mbti_data = pd.read_csv(os.path.join(data_path, filename))
 print('Total number of rows ', len(mbti_data))
 mbti_data = mbti_data.drop_duplicates(subset=['author'], keep='first') #103 authors are duplicated with different types
 print('Total number of rows after removing duplicate authors ', len(mbti_data))
-
-print('Sampling ', n_sample, ' rows...')
-mbti_data = mbti_data.sample(n=n_sample, random_state=1)
+if n_sample != 999999:
+  print('Sampling ', n_sample, ' rows...')
+  mbti_data = mbti_data.sample(n=n_sample, random_state=1)
 mbti_data = mbti_data[['author', 'comment', 'type']]
 print('======================================================')
 print('Class distribution:')
 print(mbti_data.type.value_counts())
-print('======================================================')
-print('Combine similar classes and filtering less frequent classes...')
-mbti_data['type'] = mbti_data['type'].replace(['entp','enfj','enfp','entj'], 'en')
-mbti_data['type'] = mbti_data['type'].replace([ 'infp','infj'], 'inf')
-#mbti_data.at[mbti_data['type'] == 'esfp','estp','estj','esfj'] = 'es'
+print("Removing classes with very low counts : 'esfp','estp','estj','esfj','istp','istj','isfp','isfj")
 mbti_data = mbti_data[~mbti_data['type'].isin(['esfp','estp','estj','esfj','istp','istj','isfp','isfj'])].reset_index(drop=True)
-print('New Class distribution:')
-print(mbti_data.type.value_counts())
 print('======================================================')
+if not all_class:
+  print('Combine similar classes and filtering less frequent classes...')
+  mbti_data['type'] = mbti_data['type'].replace(['entp','enfj','enfp','entj'], 'en')
+  mbti_data['type'] = mbti_data['type'].replace([ 'infp','infj'], 'inf')
+  #mbti_data.at[mbti_data['type'] == 'esfp','estp','estj','esfj'] = 'es'
+  print('New Class distribution:')
+  print(mbti_data.type.value_counts())
+  print('======================================================')
 
 #check doc length summary
 #mbti_data['len_comment'] = mbti_data.comment.apply(lambda x: docToSent(x))
@@ -51,7 +54,7 @@ print("Done..!")
 print('Class distribution after splitting:')
 print(mbti_data_1.type.value_counts())
 print('Total number of rows after splitting douments ', len(mbti_data_1))
-mbti_data_1.to_pickle(os.path.join(data_path, split_sent_filename_2))
+mbti_data_1.to_pickle(os.path.join(data_path, split_sent_filename_h))
 print('======================================================')
 
 
@@ -67,4 +70,3 @@ print(mbti_data.type.value_counts())
 
 print('Total number of rows after splitting douments ', len(mbti_data))
 mbti_data.to_pickle(os.path.join(data_path, split_sent_filename))
-
